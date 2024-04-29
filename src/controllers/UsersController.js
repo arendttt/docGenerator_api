6c4946e78748cf8b9147b079c55dc57e99b38d6e
Milena <mnarendt@rede.ulbra.br>
2024-04-29 18:27:33 -0300

    create update user feature

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -19,8 +19,12 @@ class UsersController {
       throw new AppError("CNPJ já cadastrado.")
     };
 
-    if(!email || !CNPJ) {
+    if(!email && !CNPJ) {
       throw new AppError("Você precisa preencher o email e/ou CNPJ para prosseguir.")
+    }
+
+    if(!password) {
+      throw new AppError("Você precisa definir uma senha para prosseguir.")
     }
 
     const hashedPassword = await hash(password, 8);
@@ -30,6 +34,55 @@ class UsersController {
     return response.status(201).json();
   };
 
+  async update(request, response) {
+    const { name, email, CNPJ, password, oldPassword } =  request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [ id ]);
+
+    if(!user) {
+      throw new AppError("Usuário não encontrado!")
+    };
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [ email ]);
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso!")
+    };
+
+    const userWithUpdatedCNPJ = await database.get("SELECT * FROM users WHERE CNPJ = (?)", [ CNPJ ]);
+    if(userWithUpdatedCNPJ && userWithUpdatedCNPJ.id !== user.id) {
+      throw new AppError("Este CNPJ já está cadastrado!")
+    };
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+    user.CNPJ = CNPJ ?? user.CNPJ;
+
+    if(password && oldPassword) {
+      const checkOldPassword = await compare(oldPassword, user.password);
+
+      if(!checkOldPassword) {
+        throw new AppError("A senha antiga não confere!")
+      };
+
+      user.password = await hash(password, 8);
+    }
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      CNPJ = ?,
+      password = ?,
+      updated_at = DATETIME('now')
+      WHERE id = ?`,
+      [user.name, user.email, user.CNPJ, user.password, id]
+    );
+
+    return response.status(200).json();
+  };
 };
 
 module.exports = UsersController;
